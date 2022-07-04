3d6c963161a5d70bbc75d12577cc4b4b716839f1
GustavoBauny <gustavounipam1504@gmail.com>
2022-07-04 17:53:46 -0300

    Adicionando versão final front end

--- src/App.js
+++ src/App.js
@@ -1,58 +1,64 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import AtividadeForm from './components/AtividadeForm';
 import AtividadeLista from './components/AtividadeLista';
 
-let initialState = [
-  {
-    id: 1,
-    prioridade: '1',
-    titulo: "Título",
-    descricao: "Primeira Atividade",
-  },
-  {
-    id: 2,
-    prioridade: '2',
-    titulo: "Título",
-    descricao: "Segunda Atividade",
-  },
-  {
-    id: 3,
-    prioridade: '3',
-    titulo: "Título",
-    descricao: "Terceira Atividade",
-  },
-];
-
 function App() {
-  const [atividades, setAtividades] = useState(initialState)
+  const [index, setIndex] = useState(0);
+  const [atividades, setAtividades] = useState([]);
+  const [atividade, setAtividade] = useState({ id: 0 });
 
+  useEffect(() => {
+    atividades.length <= 0
+      ? setIndex(1)
+      : setIndex(
+        Math.max.apply(
+          Math,
+          atividades.map((item) => item.id)
+        ) + 1
+      );
+  }, [atividades]);
 
-   function addAtividade(e) {
-    e.preventDefault();
-    const atividade = {
-      id: document.getElementById('id').value,
-      prioridade: document.getElementById('id').value,
-      titulo: document.getElementById('descricao').value,
-      descricao: document.getElementById('descricao').value,
-    };
-    setAtividades([...atividades, { ...atividade }]);
+  function addAtividade(ativ) {
+    setAtividades([...atividades, { ...ativ, id: index }]);
+  }
+
+  function cancelarAtividade() {
+    setAtividade({ id: 0 });
+  }
+
+  function atualizarAtividade(ativ) {
+    setAtividades(
+      atividades.map((item) => (item.id === ativ.id ? ativ : item))
+    );
+    setAtividade({ id: 0 });
   }
 
   function deletarAtividade(id) {
-    const atividadesFiltradas = atividades.filter(atividade => atividade.id !== id);
+    const atividadesFiltradas = atividades.filter(
+      (atividade) => atividade.id !== id
+    );
     setAtividades([...atividadesFiltradas]);
+  }
+
+  function pegarAtividade(id) {
+    const atividade = atividades.filter((atividade) => atividade.id === id);
+    setAtividade(atividade[0]);
   }
 
   return (
     <>
       <AtividadeForm
         addAtividade={addAtividade}
+        cancelarAtividade={cancelarAtividade}
+        atualizarAtividade={atualizarAtividade}
+        ativSelecionada={atividade}
         atividades={atividades}
       />
       <AtividadeLista
         atividades={atividades}
         deletarAtividade={deletarAtividade}
+        pegarAtividade={pegarAtividade}
       />
     </>
   );
