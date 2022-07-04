82745a14b891728f1615094c5dcbdf855e50915c
GustavoBauny <gustavounipam1504@gmail.com>
2022-07-04 12:25:39 -0300

    Adicionando hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
+import { useState } from 'react';
 import './App.css';
 
+
+
+let initialState = [
+  {
+    "id": 1,
+    "descricao": "Primeira Atividade"
+  },
+  {
+    "id": 2,
+    "descricao": "Segunda Atividade"
+  },
+];
+
 function App() {
+  const [atividades, setAtividades] = useState(initialState)
 
-  const atividades = [
-    {
-      "id": 1,
-      "descricao": "Primeira Atividade"
-    },
-    {
-      "id": 2,
-      "descricao": "Segunda Atividade"
-    },
-  ];
 
-  function addAtividade(e){
+  function addAtividade(e) {
     e.preventDefault();
     const atividade = {
-        id: document.getElementById('id').value,
-        descricao: document.getElementById('descricao').value,
+      id: document.getElementById('id').value,
+      descricao: document.getElementById('descricao').value,
     };
-    atividades.push(atividade);
-    console.log(atividades);
+    setAtividades([...atividades, { ...atividade }]);
   }
 
   return (
     <>
-    <form>
-      <input id="id" type="text" placeholder='id'></input>
-      <input id="descricao" type="text" placeholder='descricao'></input>
-      <button onClick={addAtividade}>+ Atividade</button>
-    </form>
-    <div className="mt-3">
-      <ul className="list-group">
-        {atividades.map(ativ => (
-          <li key={ativ.id} className="list-group-item">{ativ.id} - {ativ.descricao}</li>
-        ))}
-      </ul>
-    </div>
+      <form>
+        <input id="id" type="text" placeholder='id'></input>
+        <input id="descricao" type="text" placeholder='descrição'></input>
+        <button onClick={addAtividade}>+ Atividade</button>
+      </form>
+      <div className="mt-3">
+        <ul className="list-group">
+          {atividades.map(ativ => (
+            <li key={ativ.id} className="list-group-item">{ativ.id} - {ativ.descricao}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
